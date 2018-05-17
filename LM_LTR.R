95645867001955796ae7151ed2d5819ea976822c
ksapaev <39306914+ksapaev@users.noreply.github.com>
2018-05-17 13:04:23 -0400

    saving LM_LTR.R

--- LM_LTR.R
+++ LM_LTR.R
@@ -18,12 +18,11 @@ library(kernlab)
 #Creating a new dataframe by omitting NAs from 'hotel condition' column
 hc <- df[!is.na(df$Condition_Hotel_H),] 
 
-#Having both x and y of the scatterplot clean, we can proceed with plotting it.
-library(ggplot2)
+#Having both x and y of the scatterplot clean, we can proceed with plotting it by adding additional layers.
 myScatter <- ggplot(hc, aes(x=Condition_Hotel_H, y=Likelihood_Recommend_H))
-myScatter <- myScatter + geom_point() + geom_jitter(alpha=0.3, position=position_jitter(width=0.3, height=0.3))
+myScatter <- myScatter + geom_point() + geom_jitter(alpha=0.3, position=position_jitter(width=0.2, height=0.2))
 myScatter <- myScatter + scale_x_continuous( breaks = 1:10)+ scale_y_continuous( breaks = 1:10)
-myScatter <- myScatter + xlab("Hotel Condition") + ylab("Likelihood to Recommend")
+myScatter <- myScatter + xlab("Hotel Condition") + ylab("Likelihood to Recommend") + theme_classic()
 
 #Creating the png file of the scatterplot.
 png(filename="scatter_LTR.png")
