203242838c270e2952424a9ff4f1586b3953eaac
ksapaev <39306914+ksapaev@users.noreply.github.com>
2018-05-17 16:40:35 -0400

    saving SVM.R

--- SVM.R
+++ SVM.R
@@ -56,9 +56,10 @@ svmPredict <- predict(svmOut, testData, type="votes")
 
 #Creating a dataframe for prediction
 compTable <- data.frame(testData[,59], svmPredict[1,])
-
+str(svmPredict)
 #Showing predicted results in table
 table(compTable)
+str(svmPredict)
 
 #473 cases Detractor but classified as not Detractor
 #240 cases Detractor and classified as Detractor
@@ -66,6 +67,8 @@ table(compTable)
 #73 cases not Detractor but classified as Detractor
 #Accuracy is 1- (473+73)/6393 = 0.9146 or 91.46%
 
+
+#####################
 #Part C:2
 
 #Cleaning dataframe
@@ -73,33 +76,33 @@ sc <- ch[!is.na(ch$Staff_Cared_H),]
 
 
 #Creating indexes
-randomIndex <- sample(1:dim(sc)[1])
-summary(randomIndex)
+randomIndex2 <- sample(1:dim(sc)[1])
+summary(randomIndex2)
 
 #Setting a cut point
 cutPoint2_3 <- floor(2*dim(sc)[1]/3)
 cutPoint2_3
 
 #Creating a train dataframe
-trainData <- sc[randomIndex[1:cutPoint2_3],]
-str(trainData)
-length(trainData$NPS_Type)
+trainData2 <- sc[randomIndex2[1:cutPoint2_3],]
+str(trainData2)
+length(trainData2$NPS_Type)
 #Creating a test dataframe
-testData <- sc[randomIndex[(cutPoint2_3+1):dim(sc)[1]],]
-str(testData)
-length(testData$NPS_Type)
+testData2 <- sc[randomIndex2[(cutPoint2_3+1):dim(sc)[1]],]
+str(testData2)
+length(testData2$NPS_Type)
 
 #Running SVM on train data
-svmOut <- ksvm(NPS_Type ~ Condition_Hotel_H+Staff_Cared_H+Brand_PL+GP_Tier+Type_PL, data=trainData, kernel="rbfdot", kpar="automatic", C=5, prob.model=TRUE)
+svmOut2 <- ksvm(NPS_Type ~ Condition_Hotel_H+Staff_Cared_H+Brand_PL+GP_Tier+Type_PL, data=trainData2, kernel="rbfdot", kpar="automatic", C=5, prob.model=TRUE)
 
 #Predicting SVM for test data
-svmPredict <- predict(svmOut, testData, type="votes")
+svmPredict2 <- predict(svmOut2, testData2, type="votes")
 
 #Creating a dataframe for prediction
-compTable <- data.frame(testData[,59], svmPredict[1,])
+compTable2 <- data.frame(testData2[,59], svmPredict2[1,])
 
 #Showing predicted results in table
-table(compTable)
+table(compTable2)
 
 #238 cases Detractor but classified as not Detractor
 #176 cases Detractor and classified as Detractor
@@ -108,14 +111,22 @@ table(compTable)
 #Accuracy is 1- (238+50)/4033 = 0.9286 or 92.86%
 
 
+#####################
+#Part C:3
+
+#Checking 1650th survey for the first model
+compTable_1650 <- data.frame(testData[1650,59], svmPredict[1,1650])
+table(compTable_1650)
+
+#Checking 1650th survey for the second model
+compTable2_1650 <- data.frame(testData2[1650,59], svmPredict2[1,1650])
+table(compTable2_1650)
 
 
+#Predicted result is positive for both models. 1650th survey was Not Detractor.
+#Models find it as NotDetractor.
 
 
-
-#str(svmOut)
-#str(svmPredict)
-#str(svmPred)
 
 
 ## end your R code and logic 
