65e4349fd92fe0e5f34e8190653c98ab7ba04655
peaponte <31523022+peaponte@users.noreply.github.com>
2018-05-17 16:49:54 -0400

    saving ModelTest.R

--- ModelTest.R
+++ ModelTest.R
@@ -15,31 +15,6 @@ library(ggplot2)
 library(arules)
 library(kernlab)
 
-#Part A: 1
-#Creating a new dataframe by omitting NAs from 'hotel condition' column
-hc <- df[!is.na(df$Condition_Hotel_H),] 
-
-#Having both x and y of the scatterplot clean, we can proceed with plotting it by adding additional layers.
-myScatter <- ggplot(hc, aes(x=Condition_Hotel_H, y=Likelihood_Recommend_H))
-myScatter <- myScatter + geom_point() + geom_jitter(alpha=0.3, position=position_jitter(width=0.2, height=0.2))
-myScatter <- myScatter + scale_x_continuous( breaks = 1:10)+ scale_y_continuous( breaks = 1:10)
-myScatter <- myScatter + xlab("Hotel Condition") + ylab("Likelihood to Recommend") + theme_classic()
-
-#Creating the png file of the scatterplot.
-png(filename="scatter_LTR.png")
-myScatter
-dev.off()
-
-##################
-#Part A: 2
-
-# Running linear regression model
-LinMod <- lm(formula=Likelihood_Recommend_H ~ Condition_Hotel_H, data=hc)
-LinMod
-
-sc <- df[!is.na(hc$Staff_Cared_H),] 
-LinMod2 <- lm(formula=Likelihood_Recommend_H ~ Condition_Hotel_H + Staff_Cared_H, data=sc)
-LinMod2
 ####################
 
 print(summary(LinMod))
