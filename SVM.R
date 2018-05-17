dc5b2a8f463337449bff429ffc5f157ac2ca7842
ksapaev <39306914+ksapaev@users.noreply.github.com>
2018-05-17 14:29:46 -0400

    saving SVM.R

--- SVM.R
+++ SVM.R
@@ -10,6 +10,26 @@ df <- read.csv('data.csv')
 
 ## start writing your R code from here
 
+#Part C:1
+
+#Creating indexes
+randomIndex <- sample(1:dim(df)[1])
+summary(randomIndex)
+
+#Setting a cut point
+cutPoint2_3 <- floor(2*dim(df)[1]/3)
+cutPoint2_3
+
+#Creating a train dataframe
+trainData <- df[randomIndex[1:cutPoint2_3],]
+str(trainData)
+
+#Creating a test dataframe
+testData <- df[randomIndex[(cutPoint2_3+1):dim(spam)[1]],]
+str(testData)
+
+
+
 ## end your R code and logic 
 
 ####################################
