88c36a80a675e53c56ae8b67f8102f5101bb1f48
ksapaev <39306914+ksapaev@users.noreply.github.com>
2018-05-17 13:01:10 -0400

    saving LM_LTR.R

--- LM_LTR.R
+++ LM_LTR.R
@@ -10,6 +10,27 @@ df <- read.csv('data.csv')
 
 ## start writing your R code from here
 
+library(ggplot2)
+library(arules)
+library(kernlab)
+
+#Part A: 1
+#Creating a new dataframe by omitting NAs from 'hotel condition' column
+hc <- df[!is.na(df$Condition_Hotel_H),] 
+
+#Having both x and y of the scatterplot clean, we can proceed with plotting it.
+library(ggplot2)
+myScatter <- ggplot(hc, aes(x=Condition_Hotel_H, y=Likelihood_Recommend_H))
+myScatter <- myScatter + geom_point() + geom_jitter(alpha=0.3, position=position_jitter(width=0.3, height=0.3))
+myScatter <- myScatter + scale_x_continuous( breaks = 1:10)+ scale_y_continuous( breaks = 1:10)
+myScatter <- myScatter + xlab("Hotel Condition") + ylab("Likelihood to Recommend")
+
+#Creating the png file of the scatterplot.
+png(filename="scatter_LTR.png")
+myScatter
+dev.off()
+
+
 ## end your R code and logic 
 
 ####################################
