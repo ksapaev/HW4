d71b4e4d8bf0660c04acf955eafddd235fc40a15
ksapaev <39306914+ksapaev@users.noreply.github.com>
2018-05-17 14:44:04 -0400

    saving SVM.R

--- SVM.R
+++ SVM.R
@@ -12,12 +12,15 @@ df <- read.csv('data.csv')
 
 #Part C:1
 
+#Checking the structure of NPS_Type
 str(df$NPS_Type)
+#Converting as character
 df$NPS_Type <- as.character(df$NPS_Type)
+#Defining Promoters and passives as Not Detractors
 df$NPS_Type[df$NPS_Type != "Detractor"] <- "NotDetractor"
+#Formatting column as factor
 df$NPS_Type <- as.factor(df$NPS_Type)
 
-ifelse(df$NPS_Type=="Detractor", df$NPS_Type=="Detractor", df$NPS_Type=="NotDetractor" )
 
 #Creating indexes
 randomIndex <- sample(1:dim(df)[1])
