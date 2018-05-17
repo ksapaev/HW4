a9013723b9600cc3be6e33ba9bd0abbad6438f23
ksapaev <39306914+ksapaev@users.noreply.github.com>
2018-05-17 14:40:08 -0400

    saving SVM.R

--- SVM.R
+++ SVM.R
@@ -12,6 +12,13 @@ df <- read.csv('data.csv')
 
 #Part C:1
 
+str(df$NPS_Type)
+df$NPS_Type <- as.character(df$NPS_Type)
+df$NPS_Type[df$NPS_Type != "Detractor"] <- "NotDetractor"
+df$NPS_Type <- as.factor(df$NPS_Type)
+
+ifelse(df$NPS_Type=="Detractor", df$NPS_Type=="Detractor", df$NPS_Type=="NotDetractor" )
+
 #Creating indexes
 randomIndex <- sample(1:dim(df)[1])
 summary(randomIndex)
